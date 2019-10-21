bef28f1c88aedf991f32edb94c06bfbe56f0a701
chiangfire <chiang-fire@outlook.com>
2019-10-21 20:52:50 +0800

    feat: add dodo

---
+++ dodo/dodo-biz/src/main/java/com/firecode/springcloudtest/dodo/biz/App.java
@@ -0,0 +1,13 @@
+package com.firecode.springcloudtest.dodo.biz;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
